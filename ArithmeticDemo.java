98a029a4d744c1152ccb91f901e0a2080418153a
mykelson <mykelsure@gmail.com>
2019-05-07 15:12:24 +0100

    first commit

---
+++ ArithmeticDemo.java
@@ -0,0 +1,19 @@
+class arithemeticDemo {
+    public static void main(String args[]) {
+        int result = 1 + 2;
+        System.out.println(result);
+        
+        result = result - 1;
+        System.out.println(result);
+
+        result = result * 2;
+        System.out.println(result);
+        
+        result = result / 2;
+        System.out.println(result);
+
+        result = result + 8;
+        result = result % 7;
+        System.out.println(result);
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Arithmetic
+This is a Java program that implements Java's arithemetic operators
+
+## Project status
+Project development has been temporarily paused.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
